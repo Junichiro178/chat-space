faf18e7734ec8dbb2cb55c386fe7d44a174d2f26
Junichiro178 <bz.kitm.fj@gmail.com>
2020-07-18 15:27:46 +0900

    Fixed type error in messages_controlle & messages.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -63,23 +63,28 @@ $(function(){
   })
 
   var reloadMessages = function() {
-    var lasr_message_id = $('.message:last').data("message-id");
+    var last_message_id = $('.message:last').data("message-id");
     $.ajax({
       url: "api/messages",
-      type: "GET",
+      type: "get",
       dataType: "json",
       data: {id: last_message_id}
     })
     .done(function(messages) {
-      var insertHTML = '';
-      $.each(messages, function(i, message) {
-        insertHTML += buildHTML(message)
-      });
-      $('.messages').append(insertHTML);
+      if (messages.length !== 0) {
+        var insertHTML = '';
+        $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message)
+        });
+        $('.chat-main__message-list').append(insertHTML);
+        $('.chat-main__message-list').animate({ scrollTop: $('.chat-main__message-list')[0].scrollHeight});
+      }
     })
     .fail(function() {
       alert('error');
     })
-    setInterval(reloadMessages, 7000);
   };
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
 });
